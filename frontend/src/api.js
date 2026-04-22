e5b31cf48639d90f8b9cc11e8e8a24b318ebf61d
parasgak <parasgakhar101@gmail.com>
2026-04-23 01:23:33 +0530

    add home route message

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://loginpage-lwbk.onrender.com/api',
+    baseURL: 'http://localhost:5000/api',
 });
 
 // Automatically add token to headers if it exists
